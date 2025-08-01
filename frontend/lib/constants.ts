95424d1b619961403aa4599ca5dfe0fb1a77ef0a
Aarush Kumar <2005aarushkumar@gmail.com>
2025-08-02 02:36:52 +0530

    feat: init shadcn/ui

---
+++ frontend/lib/constants.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = "StoryCraftAI";
+
+export const APP_DESCRIPTION =
+    "Create Short Story Videos with Realistic AI Voices. Transform your words into visual stories with AI. Choose your favorite voice, pick an image, and generate a complete video that tells your story with emotion and clarity.";
